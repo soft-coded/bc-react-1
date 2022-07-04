fd92038a094efc66379b0f773d3ec93b0fb7dbad
Prashu <Prashanth.P@stackroute.in>
2022-07-04 11:55:08 +0530

    Initial comit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+
+function App() {
+  return (
+    <div>
+      <h1>learn react</h1>
+      {/* Header Component */}
+      {/* Body Component */}
+      {/* Footer Component */}
+    </div>
+  );
+}
+
+export default App;
