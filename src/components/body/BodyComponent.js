332521d67f3f9acd8e80991347b8d82eb06535da
shrutanten <shrutanten.work@gmail.com>
2023-09-02 00:35:54 +0530

    Add body components to body

--- src/components/body/BodyComponent.js
+++ src/components/body/BodyComponent.js
@@ -1,3 +1,12 @@
+import BodyLeftComponent from "./body-left/BodyLeftComponent";
+import BodyRightComponent from "./body-right/BodyRightComponent";
+
 export default function BodyComponent() {
-	return <h2>I am Body Component !!!</h2>;
+	return (
+		<>
+			<h2>I am Body Component !!!</h2>;
+			<BodyLeftComponent />
+			<BodyRightComponent />
+		</>
+	);
 }
